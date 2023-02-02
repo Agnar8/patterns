a13cccbc54bc6516b38bfd2eda377f7bb3994949
Gubarev <mg@sendpulse.com>
2023-02-02 23:23:47 +0200

    Add pattern Simple factory

---
+++ Generative/Simple factory/index.php
@@ -0,0 +1,37 @@
+<?php
+
+class Factory
+{
+    public static function build($type)
+    {
+        $class = "Format" . $type;
+        if (!class_exists($class)) {
+            throw new Exception("Missing format class.");
+        }
+        return new $class;
+    }
+}
+
+interface FormatInterface
+{
+}
+
+class FormatString implements FormatInterface
+{
+}
+
+class FormatNumber implements FormatInterface
+{
+}
+
+try {
+    $string = Factory::build("String");
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+try {
+    $number = Factory::build("Number");
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
